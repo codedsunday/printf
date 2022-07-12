49cbe90fe8986ed133e4ee3679268503db2606ff
codedsunday <meisterji77@yahoo.com>
2022-07-12 03:39:31 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,50 +1,48 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
-  *_printf - prints formatted output.
-  *@format: input.
-  *
-  *Return: number of chars printed or -1.
-  */
+ * _printf - main printf function
+ * @format: string parameter
+ *
+ * Return: count of characters in @format
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i, len;
-	int (*get_ptr)(va_list, int);
+	const char *str;
+	int (*func)(va_list, flag_t *, mod_t *);
+	flag_t f = {0, 0, 0};
+	mod_t m = {0, 0, 0};
+	int i, count = 0, num = 0;
 
 	va_start(args, format);
-	if (!(format))
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	i = 0;
-	len = 0;
-	while (format && format[i])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (str = format; *str; str++)
 	{
-		if (format[i] == '%')
+		if (*str == '%')
 		{
-			i++;
-			if (format[i] == '%')
+			str++;
+			if (*str == '%')
 			{
-				len += _putchar(format[i]);
-				i++;
+				count += _putchar('%');
 				continue;
 			}
-			if (format[i] == '\0')
-				return (-1);
-			get_ptr = get_print_func(format[i]);
-			if (get_ptr != NULL)
-				len = get_ptr(args, len);
-			else
-			{
-				len += _putchar(format[i - 1]);
-				len += _putchar(format[i]);
-			}
-			i++;
-		}
-		else
-		{
-			len += _putchar(format[i]);
-			i++;
-		}
+			for (; get_flags(*str, &f); str++)
+			{}
+			for (i = 0; *str >= 48 && *str <= 57; str++, i++)
+				num = (num * (i * 10)) + (*str - '0');
+			set_width(num, &m);
+			for (; get_modifier(*str, &m); str++)
+			{}
+			func = get_func(*str);
+			count += (func) ? func(args, &f, &m)
+					: _printf("%%%c", *str);
+		} else
+			count += _putchar(*str);
 	}
-	va_end(args);
-	return (len);
+	_putchar(-1), va_end(args);
+	return (count);
 }
